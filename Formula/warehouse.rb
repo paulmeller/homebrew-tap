6d80524be9bd451e0bcf6fb83bf5776da1101f8b
Paul Meller <14568+paulmeller@users.noreply.github.com>
2026-03-14 14:07:33 +1100

    Add sha256 checksums to warehouse formula

--- Formula/warehouse.rb
+++ Formula/warehouse.rb
@@ -7,10 +7,10 @@ class Warehouse < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/paulmeller/warehouse-cli/releases/download/v0.3.2/warehouse-macos-arm64.tar.gz"
-      sha256 ""
+      sha256 "58d954e387e194c233b49686993c3116c99d4a05fb02c42cc6e1e1e67bf95e50"
     else
       url "https://github.com/paulmeller/warehouse-cli/releases/download/v0.3.2/warehouse-macos-x86_64.tar.gz"
-      sha256 ""
+      sha256 "eab29d6cc8eda365f2c5aec6915e240a1d8042abe39f2fb867b340e5ee69b9c1"
     end
   end
 
